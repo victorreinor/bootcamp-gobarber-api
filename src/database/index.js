0af6bef598184610547b32d6c93f130c9d85bac4
Victor Reinor <victorreinor@gmail.com>
2019-07-02 20:48:55 -0300

    adiciona o file para passar nos models

--- src/database/index.js
+++ src/database/index.js
@@ -1,8 +1,9 @@
 import Sequelize from 'sequelize';
 import databaseConfig from '../config/database';
 import User from '../app/models/User';
+import File from '../app/models/File';
 
-const models = [User];
+const models = [User, File];
 
 class Database {
   constructor() {
@@ -12,7 +13,9 @@ class Database {
   init() {
     this.conection = new Sequelize(databaseConfig);
 
-    models.map(model => model.init(this.conection));
+    models
+      .map(model => model.init(this.conection))
+      .map(model => model.associate && model.associate(this.conection.models));
   }
 }
 
