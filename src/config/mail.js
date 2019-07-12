5270620e820c978b403f30ff7c0ec599e611ddc0
Victor Reinor <victorreinor@gmail.com>
2019-07-11 22:23:22 -0300

    adiciona as chamadas da dependencia dotenv para setar na process.env e passa as configuracoes no seu devido lugar

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: 'a624db6b97d5d5',
-    pass: '37a367d8ee0ff1',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe GoBarber <noreply@gobarber.com>',
--- src/config/sentry.js
+++ src/config/sentry.js
@@ -1,3 +1,3 @@
 export default {
-  dsn: 'https://2df25da1f15e4775bf9fb3f27d49aadd@sentry.io/1503136',
+  dsn: process.env.SENTRY_DSN,
 };
